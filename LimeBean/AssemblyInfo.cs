b87d6395e0d00c286ccdbfd64b28343287fdbc2d
AlekseyMartynov <bclexa@gmail.com>
2014-07-22 23:56:10 +0400

    license and copyright added

--- LimeBean/AssemblyInfo.cs
+++ LimeBean/AssemblyInfo.cs
@@ -2,4 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("LimeBean.Tests")]
+
+[assembly: AssemblyProduct("LimeBean")]
 [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Aleksey Martynov")]
