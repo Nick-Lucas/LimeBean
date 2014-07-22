f19365de73f1128d95b75b47eb1f9d10a54453f8
AlekseyMartynov <bclexa@gmail.com>
2014-07-22 23:04:57 +0400

    Northwind example added

---
+++ LimeBean.Tests/Examples/Northwind.cs
@@ -0,0 +1,173 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace LimeBean.Tests.Examples {
+
+    [TestFixture, Explicit]
+    public class Northwind {
+        // NOTE in web apps BeanApi must have per-request life-time
+        // For example, in ASP.NET it should live in HttpContext.Items
+        public static BeanApi R { get; set; }
+
+        [SetUp]
+        public void SetUp() {
+            R = new BeanApi("data source=:memory:", SQLiteFactory.Instance);
+            R.EnterFluidMode();
+        }
+
+        [TearDown]
+        public void TearDown() {
+            R.Dispose();
+        }
+
+        class Category : Bean {
+
+            public Category()
+                : base("category") {
+            }
+
+            // Typed property accessors
+
+            public string Name {
+                get { return Get<string>("name"); }
+                set { Put("name", value); }
+            }
+
+            public string Description {
+                get { return Get<string>("description"); }
+                set { Put("description", value); }
+            }
+
+            bool HasName { get { return !String.IsNullOrWhiteSpace(Name); } }
+
+            // Helper method to find all products in this category
+            // NOTE internal LRU cache is used, so DB is not hit every time
+            public Product[] GetProducts() {
+                return R.Find<Product>("where category_id = ?", ID);
+            }
+
+            // Validation rules prevent storing of unnamed categories
+            protected internal override void BeforeStore() {
+                if(!HasName)
+                    throw new Exception("Category name cannot be empty");
+            }
+
+            // Cascading delete of all its products 
+            // NOTE deletion is wrapped in an implicit transaction
+            // so that consistency is maintained
+            protected internal override void BeforeTrash() {
+                foreach(var p in GetProducts())
+                    R.Trash(p);
+            }
+
+            public override string ToString() {
+                if(HasName)
+                    return Name;
+
+                return base.ToString();
+            }
+        }
+
+        class Product : Bean {
+
+            public Product()
+                : base("product") {
+            }
+
+            // Typed accessors
+
+            public string Name {
+                get { return Get<string>("name"); }
+                set { Put("name", value); }
+            }
+
+            public long? CategoryID {
+                get { return Get<long>("category_id"); }
+                set { Put("category_id", value); }
+            }
+
+            public decimal UnitPrice {
+                get { return Get<decimal>("unit_price"); }
+                set { Put("unit_price", value); }
+            }
+
+            public bool Discontinued {
+                get { return Get<bool>("discontinued"); }
+                set { Put("discontinued", value); }
+            }
+
+            // Example of a referenced bean
+            // NOTE internal LRU cache is used during loading
+            public Category Category {
+                get { return R.Load<Category>(CategoryID.Value); }
+                set { CategoryID = value.ID; }
+            }
+
+            // A set of validation checks
+            protected internal override void BeforeStore() {
+                if(String.IsNullOrWhiteSpace(Name))
+                    throw new Exception("Product name cannot be empty");
+
+                if(UnitPrice <= 0)
+                    throw new Exception("Price must be a non-negative number");
+
+                if(Category == null)
+                    throw new Exception("Product must belong to an existing category");
+            }
+        }
+
+
+        [Test]
+        public void Scenario() {
+            var beverages = new Category { 
+                Name = "Beverages",
+                Description = "Soft drinks, coffees, teas, beers, and ales"
+            };
+
+            var condiments = new Category {
+                Name = "Condiments",
+                Description = "Sweet and savory sauces, relishes, spreads, and seasonings"
+            };
+
+            R.Store(beverages);
+            R.Store(condiments);
+
+
+            var chai = new Product { 
+                Name = "Chai",
+                UnitPrice = 18,
+                Category = beverages
+            };
+
+            var chang = new Product {
+                Name = "Chang",
+                UnitPrice = 19,
+                Category = beverages
+            };
+
+            var syrup = new Product {
+                Name = "Aniseed Syrup",
+                UnitPrice = 9.95M,
+                Category = condiments,
+                Discontinued = true
+            };
+
+            R.Store(chai);
+            R.Store(chang);
+            R.Store(syrup);
+
+            Console.WriteLine("Number of known beverages: {0}", beverages.GetProducts().Length);
+
+            Console.WriteLine("Deleting all the beverages...");
+            R.Trash(beverages);
+
+            Console.WriteLine("Products remained: {0}", R.Find<Product>().Length);
+        }
+
+    }
+
+}
