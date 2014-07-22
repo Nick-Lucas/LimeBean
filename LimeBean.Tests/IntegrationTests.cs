91f46282a9072c4ce0f217a8679553e4fb431fb1
AlekseyMartynov <bclexa@gmail.com>
2014-07-22 23:08:26 +0400

    Crud example moved

--- LimeBean.Tests/IntegrationTests.cs
+++ LimeBean.Tests/IntegrationTests.cs
@@ -54,51 +54,6 @@ namespace LimeBean.Tests {
             }
         }
 
-        [Test, Explicit]
-        public void Example_CRUD() {
-            // Based on https://code.google.com/p/orange-bean/#CRUD
-
-            // Tell about your database
-            var R = new BeanApi("data source=:memory:", new SQLiteFactory());
-
-            // Enable automatic schema update
-            R.EnterFluidMode();
-            
-            // create a bean 
-            var bean = R.Dispense("person");
-
-            // it's of kind "person"
-            Console.WriteLine(bean.GetKind());
-
-            // fill it
-            bean["name"] = "Alex";
-            bean["year"] = 1984;
-            bean["smart"] = true;
-
-            // store it
-            var id = R.Store(bean);
-
-            // Database schema will be updated automatically for you
-
-            // Now the bean has an id
-            Console.WriteLine(bean.ID);
-            
-            // load a bean
-            bean = R.Load("person", id);
-
-            // change it
-            bean["name"] = "Lexa";
-            bean["new_prop"] = 123;
-
-            // commit changes
-            R.Store(bean);
-
-            // or delete it
-            R.Trash(bean);
-
-            // close the connection
-            R.Dispose();
-        }
     }
 
 }
