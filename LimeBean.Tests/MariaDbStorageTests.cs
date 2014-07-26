d5b8e0dd5e3accaebc6f18ba1708acd65a47464d
AlekseyMartynov <bclexa@gmail.com>
2014-07-26 09:56:37 +0400

    mariadb: schema reading

---
+++ LimeBean.Tests/MariaDbStorageTests.cs
@@ -0,0 +1,131 @@
+﻿using MySql.Data.MySqlClient;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LimeBean.Tests {
+
+    [TestFixture]
+    public class MariaDbStorageTests {
+        IDbConnection _conn;
+        IDatabaseAccess _db;
+        DatabaseStorage _storage;
+
+        [TestFixtureSetUp]
+        public void CommonSetUp() {
+            _conn = new MySqlConnection("server=localhost; uid=root; pwd=qwerty");
+            _conn.Open();
+
+            _db = new DatabaseAccess(_conn);
+            _db.Exec("set sql_mode=STRICT_TRANS_TABLES");
+        }
+
+        [SetUp]
+        public void SetUp() {
+            const string dbname = "lime_bean_tests";
+            _db.Exec("drop database if exists " + dbname);
+            _db.Exec("create database " + dbname);
+            _db.Exec("use " + dbname);
+
+            _storage = new MariaDbStorage(_db);
+        }
+
+        [TestFixtureTearDown]
+        public void CommonTearDown() {
+            _conn.Dispose();
+        }
+
+
+        [Test]
+        public void Schema() {
+            _db.Exec(@"create table t(
+                pk integer primary key,
+
+                ti1 TinyInt(123),
+                ti2 TINYINT,
+                ti3 bool,
+                ti4 Boolean, 
+
+                i1  integer(123),
+                i2  Integer,
+                i3  INT,
+
+                bi1 bigint(123),
+                bi2 BIGINT,
+
+                d1  Double,
+                d2  DOUBLE PRECISION,
+
+                t1  varchar(32),
+                t2  VarChar(255),
+                t3  TinyText,
+                t4  Text,
+                t5  MEDIUMTEXT,
+
+                x1  smallint,
+                x2  mediumint,
+                x3  double(3,2),
+                x4  float,
+                x6  decimal,
+                x7  date,
+                x8  timestamp,
+                x9  char(32),
+                x10 varchar(123),
+                x11 binary,
+                x12 blob,
+                x13 longtext,
+
+                x14 int unsigned,
+                x15 int not null,
+                x16 int default '123'
+            )");
+
+            var schema = _storage.GetSchema();
+            Assert.AreEqual(1, schema.Count);
+
+            var t = schema["t"];
+            Assert.IsFalse(t.ContainsKey("pk"));
+
+            Assert.AreEqual(MariaDbStorage.RANK_INT8, t["ti1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT8, t["ti2"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT8, t["ti3"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT8, t["ti4"]);
+
+            Assert.AreEqual(MariaDbStorage.RANK_INT32, t["i1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT32, t["i2"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT32, t["i3"]);
+
+            Assert.AreEqual(MariaDbStorage.RANK_INT64, t["bi1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_INT64, t["bi2"]);
+
+            Assert.AreEqual(MariaDbStorage.RANK_DOUBLE, t["d1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_DOUBLE, t["d2"]);
+
+            Assert.AreEqual(MariaDbStorage.RANK_TEXT5, t["t1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_TEXT8, t["t2"]);
+            Assert.AreEqual(MariaDbStorage.RANK_TEXT8, t["t3"]);
+            Assert.AreEqual(MariaDbStorage.RANK_TEXT16, t["t4"]);
+            Assert.AreEqual(MariaDbStorage.RANK_TEXT24, t["t5"]);
+
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x1"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x2"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x3"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x4"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x6"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x7"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x8"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x9"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x10"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x11"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x12"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x13"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x14"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x15"]);
+            Assert.AreEqual(MariaDbStorage.RANK_CUSTOM, t["x16"]);
+        }
+    }
+
+}
---
+++ LimeBean/MariaDbStorage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LimeBean {
+
+    using TableColumns = Dictionary<string, int>;
+    using Schema = Dictionary<string, Dictionary<string, int>>;
+
+    class MariaDbStorage : DatabaseStorage {
+        internal const int
+            RANK_INT8 = 0,
+            RANK_INT32 = 1,
+            RANK_INT64 = 2,
+            RANK_DOUBLE = 3, // TODO 
+            RANK_TEXT5 = 4,
+            RANK_TEXT8 = 5,
+            RANK_TEXT16 = 6,
+            RANK_TEXT24 = 7;
+
+        public MariaDbStorage(IDatabaseAccess db)
+            : base(db) {
+            db.Exec("set names utf8");
+        }
+
+        protected override int GetRankFromValue(IConvertible value) {
+            throw new NotImplementedException();
+        }
+
+        protected override int GetRankFromSqlType(string sqlType) {
+            if(sqlType.Contains("unsigned"))
+                return RANK_CUSTOM;
+
+            if(sqlType.StartsWith("tinyint("))
+                return RANK_INT8;
+
+            if(sqlType.StartsWith("int("))
+                return RANK_INT32;
+
+            if(sqlType.StartsWith("bigint("))
+                return RANK_INT64;
+
+            if(sqlType == "double")
+                return RANK_DOUBLE;
+
+            if(sqlType == "varchar(32)")
+                return RANK_TEXT5;
+
+            if(sqlType == "varchar(255)" || sqlType == "tinytext")
+                return RANK_TEXT8;
+
+            if(sqlType == "text")
+                return RANK_TEXT16;
+
+            if(sqlType == "mediumtext")
+                return RANK_TEXT24;
+
+            return RANK_CUSTOM;
+        }
+
+        protected override string GetSqlTypeFromRank(int rank) {
+            throw new NotImplementedException();
+        }
+
+        protected override Schema LoadSchema() {
+            var result = new Schema();
+
+            foreach(var tableName in Db.Col<string>(false, "show tables")) {
+                var columns = new TableColumns();
+                foreach(var row in Db.Rows(false, "show columns from " + QuoteName(tableName))) {
+                    if("PRI".Equals(row["Key"]))
+                        continue;
+
+                    var allowsNull = "YES".Equals(row["Null"]);
+                    var hasDefault = row["Default"] != null;
+
+                    columns[Convert.ToString(row["Field"])] = !allowsNull || hasDefault
+                        ? RANK_CUSTOM
+                        : GetRankFromSqlType(Convert.ToString(row["Type"]));
+                }
+                result[tableName] = columns;
+            }
+
+            return result;
+        }
+
+        protected override void UpdateSchema(string kind, TableColumns oldColumns, TableColumns changedColumns, TableColumns addedColumns) {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetPrimaryKeySqlType() {
+            throw new NotImplementedException();
+        }
+
+        public override long GetLastInsertID() {
+            throw new NotImplementedException();
+        }
+    }
+
+}
