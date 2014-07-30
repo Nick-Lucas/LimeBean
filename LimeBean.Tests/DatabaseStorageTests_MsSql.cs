462949e10a18222009aab425f73235b6ec762b42
AlekseyMartynov <bclexa@gmail.com>
2014-07-30 22:32:14 +0400

    mssql: schema reading

---
+++ LimeBean.Tests/DatabaseStorageTests_MsSql.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace LimeBean.Tests {
+
+    [TestFixture]
+    public class DatabaseStorageTests_MsSql {
+        static string TEMP_DB_NAME = "lime_bean_" + Guid.NewGuid().ToString("N");
+
+        IDbConnection _conn;
+        IDatabaseAccess _db;
+        DatabaseStorage _storage;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp() {
+            var server = Environment.GetEnvironmentVariable("LIME_TEST_SQLSERVER") ?? ".\\SQLEXPRESS";
+            _conn = new SqlConnection("server=" + server + "; user instance=true; integrated security=true;");
+            _conn.Open();
+        }
+
+        [SetUp]
+        public void SetUp() {
+            IDatabaseDetails details = new MsSqlDetails();
+            IDatabaseAccess db = new DatabaseAccess(_conn, details);
+            DatabaseStorage storage = new DatabaseStorage(details, db);
+
+            db.Exec("create database " + TEMP_DB_NAME);
+            db.Exec("use " + TEMP_DB_NAME);
+
+            _db = db;
+            _storage = storage;
+        }
+
+        [TearDown]
+        public void TearDown() {
+            _db.Exec("alter database " + TEMP_DB_NAME + " set offline");
+            _db.Exec("drop database " + TEMP_DB_NAME);
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown() {
+            _conn.Dispose();
+        }
+
+
+        [Test]
+        public void Schema() {
+            _db.Exec(@"create table foo(
+                id  int,
+
+                b   tinyint,
+                i   int,
+                l   bigint,
+                d   float(53),
+                t1  nvarchar(32),
+                t2  nvarchar(4000),
+                t3  nvarchar(MAX),
+
+                x1  bit,
+                x2  decimal,
+                x3  float(10),
+                x4  varchar(32),
+                x5  nvarchar(33),
+
+                x6  int not null,
+                x7  int default 123
+            )");
+
+            var schema = _storage.GetSchema();
+            Assert.AreEqual(1, schema.Count);
+
+            var cols = schema["foo"];
+            Assert.IsFalse(cols.ContainsKey("id"));
+
+            Assert.AreEqual(MsSqlDetails.RANK_BYTE, cols["b"]);
+            Assert.AreEqual(MsSqlDetails.RANK_INT32, cols["i"]);
+            Assert.AreEqual(MsSqlDetails.RANK_INT64, cols["l"]);
+            Assert.AreEqual(MsSqlDetails.RANK_DOUBLE, cols["d"]);
+            Assert.AreEqual(MsSqlDetails.RANK_TEXT_32, cols["t1"]);
+            Assert.AreEqual(MsSqlDetails.RANK_TEXT_4000, cols["t2"]);
+            Assert.AreEqual(MsSqlDetails.RANK_TEXT_MAX, cols["t3"]);
+
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x1"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x2"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x3"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x4"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x5"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x6"]);
+            Assert.AreEqual(CommonDatabaseDetails.RANK_CUSTOM, cols["x7"]);
+        }
+    }
+
+}
---
+++ LimeBean/MsSqlDetails.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LimeBean {
+
+    class MsSqlDetails : IDatabaseDetails {
+
+        public const int
+            RANK_BYTE = 0,
+            RANK_INT32 = 1,
+            RANK_INT64 = 2,
+            RANK_DOUBLE = 3,
+            RANK_TEXT_32 = 4,
+            RANK_TEXT_4000 = 5,
+            RANK_TEXT_MAX = 6;
+
+        public string DbName {
+            get { return "MsSql"; }
+        }
+
+        public string PrimaryKeySqlType {
+            get { return "bigint identity(1,1) primary key"; }
+        }
+
+        public string GetParamName(int index) {
+            return "@p" + index;
+        }
+
+        public string QuoteName(string name) {
+            if(name.Contains("]"))
+                throw new ArgumentException();
+
+            return "[" + name + "]";
+        }
+
+        public void ExecInitCommands(IDatabaseAccess db) {
+        }
+
+        public long GetLastInsertID(IDatabaseAccess db) {
+            return db.Cell<long>(false, "select @@IDENTITY");
+        }
+
+        public string GetCreateTableStatementPostfix() {
+            return null;
+        }
+
+        public int GetRankFromValue(IConvertible value) {
+            throw new NotImplementedException();
+        }
+
+        public int GetRankFromSqlType(string sqlType) {
+            if(sqlType.StartsWith("48:"))
+                return RANK_BYTE;
+
+            if(sqlType.StartsWith("56:"))
+                return RANK_INT32;
+
+            if(sqlType.StartsWith("127:"))
+                return RANK_INT64;
+
+            switch(sqlType) { 
+                case "62:8":
+                    return RANK_DOUBLE;
+
+                case "231:64":
+                    return RANK_TEXT_32;
+
+                case "231:8000":
+                    return RANK_TEXT_4000;
+
+                case "231:-1":
+                    return RANK_TEXT_MAX;
+            }
+
+            return CommonDatabaseDetails.RANK_CUSTOM;
+        }
+
+        public string GetSqlTypeFromRank(int rank) {
+            throw new NotImplementedException();
+        }
+
+        public IConvertible ConvertLongValue(long value) {
+            if(0L <= value && value <= 255L)
+                return Convert.ToByte(value);
+
+            if(-0x80000000L <= value && value <= 0x7FFFFFFFL)
+                return Convert.ToInt32(value);
+
+            return value;
+        }
+
+        public string[] GetTableNames(IDatabaseAccess db) {
+            return db.Col<string>(false, "select name from sys.objects where type='U'");
+        }
+
+        public IDictionary<string, IConvertible>[] GetColumns(IDatabaseAccess db, string tableName) {
+            return db.Rows(false, "select name, system_type_id, max_length, is_nullable, object_definition(default_object_id) [default] from sys.columns where object_id = object_id({0})", tableName);
+        }
+
+        public bool IsNullableColumn(IDictionary<string, IConvertible> column) {
+            return column["is_nullable"].ToBoolean(CultureInfo.InvariantCulture);
+        }
+
+        public IConvertible GetColumnDefaultValue(IDictionary<string, IConvertible> column) {
+            return column["default"];
+        }
+
+        public string GetColumnName(IDictionary<string, IConvertible> column) {
+            return column["name"].ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string GetColumnType(IDictionary<string, IConvertible> column) {
+            return String.Concat(column["system_type_id"], ":", column["max_length"]);
+        }
+
+        public void UpdateSchema(IDatabaseAccess db, string tableName, IDictionary<string, int> oldColumns, IDictionary<string, int> changedColumns, IDictionary<string, int> addedColumns) {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReadOnlyCommand(string text) {
+            return Regex.IsMatch(text, @"^\s*select\s", RegexOptions.IgnoreCase);
+        }
+    }
+
+}
