bfae60ebeffc459af3ea14ea3849ff4e48400fce
AlekseyMartynov <bclexa@gmail.com>
2015-07-23 02:13:33 +0300

    dnx build script

--- appveyor-dnx.cmd
+++ appveyor-dnx.cmd
@@ -5,10 +5,10 @@ if _%1==_ (
     exit /b 1
 )
 
-xcopy /I System.Data.SQLite\x86 LimeBean.Tests\x86
-xcopy /I System.Data.SQLite\x64 LimeBean.Tests\x64
+xcopy /Y /I System.Data.SQLite\x86 LimeBean.Tests\x86
+xcopy /Y /I System.Data.SQLite\x64 LimeBean.Tests\x64
 
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}" || exit /b 1
-call dnvm install latest -r %1 || exit /b 1
+call %userprofile%\.dnx\bin\dnvm install latest -r %1 || exit /b 1
 call dnu restore || exit /b 1
 dnx LimeBean.Tests xunit.runner.dnx || exit /b 1
