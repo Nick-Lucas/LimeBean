f3ea813021397cce1f2b6fbeab60c0a8a9af3ef3
AlekseyMartynov <bclexa@gmail.com>
2015-07-29 23:40:57 +0300

    better package meta

--- LimeBean.NuGet/write-meta.ps1
+++ LimeBean.NuGet/write-meta.ps1
@@ -1,12 +1,11 @@
 param ([string]$path)
 
-$meta_version = "0.3.1"
-$meta_description = "RedBeanPHP-inspired data access layer for .NET, .NET Core (DNX/DNXCore), Mono and Xamarin"
+$meta_version = "0.3.2"
+$meta_description = "RedBeanPHP-inspired data access layer"
 $meta_author = "Aleksey Martynov"
 $meta_copyright = "Copyright (c) 2014-$(get-date -format yyyy) $meta_author"
 $meta_project_url = "https://github.com/AlekseyMartynov/LimeBean"
 $meta_license_url = "https://raw.githubusercontent.com/AlekseyMartynov/LimeBean/master/LICENSE.txt"
-$meta_tags = "orm", "sqlite", "mysql", "dnx", "monoandroid", "monotouch", "xamarin"
 
 (Get-Content $path) | Foreach-Object {
     $_  -replace '(AssemblyVersion.+?)[\d.]+', "`${1}%lime_version%" `
@@ -17,7 +16,5 @@ $meta_tags = "orm", "sqlite", "mysql", "dnx", "monoandroid", "monotouch", "xamar
         -replace '%lime_author%', $meta_author `
         -replace '%lime_copyright%', $meta_copyright `
         -replace '%lime_project_url%', $meta_project_url `
-        -replace '%lime_license_url%', $meta_license_url `
-        -replace '%lime_tags%', ($meta_tags -join ' ') `
-        -replace '%lime_tags_json%', ($meta_tags -join '", "')
+        -replace '%lime_license_url%', $meta_license_url
 } | Set-Content $path
