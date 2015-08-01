6758d1c8d1f45edaccbf7ffbe225ed677f190d80
AlekseyMartynov <bclexa@gmail.com>
2015-08-01 20:49:29 +0300

    xamarin sln moved to folder

--- LimeBean.NuGet/build.cmd
+++ LimeBean.NuGet/build.cmd
@@ -14,7 +14,7 @@ for %%f in (%META_FILES%) do (
 )
 
 call dnu pack ..\LimeBean --configuration Release --quiet || goto error
-%MSBUILD% ..\LimeBean.Xamarin.sln || goto error
+%MSBUILD% ..\LimeBean.Xamarin\LimeBean.Xamarin.sln || goto error
 %NUGET% pack LimeBean.Xamarin.nuspec || goto error
 copy ..\LimeBean\bin\Release\*.nupkg . || goto error
 del *symbols*.nupkg
