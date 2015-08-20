58976dd5a92906efd423aa11f6fcb79447d1d568
AlekseyMartynov <bclexa@gmail.com>
2015-08-20 23:27:09 +0300

    nuget packaging improvements

--- LimeBean.NuGet/build.cmd
+++ LimeBean.NuGet/build.cmd
@@ -1,9 +1,12 @@
 @echo off
 
-set NUGET=c:\my\apps\nuget
 set MSBUILD=c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release /v:m
 set META_FILES=..\LimeBean\AssemblyInfo.cs ..\LimeBean.Dnx\LimeBean\project.json LimeBean.Xamarin.nuspec
 set RESULT=ok
+
+if not exist nuget.exe (
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', 'nuget.exe')"
+)
 
 rd /s/q ..\LimeBean.Dnx\LimeBean\bin
 del *.nupkg
@@ -13,9 +16,10 @@ for %%f in (%META_FILES%) do (
     powershell -ExecutionPolicy RemoteSigned -File write-meta.ps1 %%f
 )
 
+call dnu restore ..\LimeBean.Dnx\LimeBean --quiet || goto error
 call dnu pack ..\LimeBean.Dnx\LimeBean --configuration Release --quiet || goto error
 %MSBUILD% ..\LimeBean.Xamarin\LimeBean.Xamarin.sln || goto error
-%NUGET% pack LimeBean.Xamarin.nuspec || goto error
+nuget pack LimeBean.Xamarin.nuspec || goto error
 copy ..\LimeBean.Dnx\LimeBean\bin\Release\*.nupkg . || goto error
 del *symbols*.nupkg
 
@@ -34,3 +38,5 @@ if not %RESULT%==ok (
     echo ERRORS!
     exit /b 1
 )
+
+echo SUCCESS!
