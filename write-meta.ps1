ba00d0afe0939b6933933ec3d284f20a36989f98
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2016-06-28 16:18:19 +0300

    Update NET Core projects (#10)
    * revert lime_name param because it breaks assembly name when built locally
    
    * update test proj to rtm dotnet and preview2 tooling
    
    * use official preview2 installer

--- write-meta.ps1
+++ write-meta.ps1
@@ -15,7 +15,6 @@ if ($tag -match '^v?(([.\d]+)[\w-]*)$')  {
     $meta_version_full = $meta_version_numeric
 }
 
-$meta_name = "LimeBean"
 $meta_description = "RedBeanPHP-inspired data access layer"
 $meta_author = "Nick Lucas"
 $meta_copyright = "Copyright (c) 2014-2016 Aleksey Martynov, 2016-$(get-date -format yyyy) $meta_author"
@@ -28,7 +27,6 @@ $meta_license_url = "https://raw.githubusercontent.com/Nick-Lucas/LimeBean/maste
     (Get-Content $path) | %{
         $_  -replace '(AssemblyVersion.+?")[^"]+', "`${1}$meta_version_numeric" `
             -replace '("version":.+?")[^"]+', "`${1}$meta_version_full" `
-            -replace '%lime_name%', $meta_name `
             -replace '%lime_description%', $meta_description `
             -replace '%lime_author%', $meta_author `
             -replace '%lime_copyright%', $meta_copyright `
